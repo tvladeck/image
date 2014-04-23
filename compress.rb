0a1d1e8a3e6ab400482d23af86594d4cd2966ccc
Tom Vladeck <thomas.vladeck@gmail.com>
2014-04-23 09:28:08 -0700

    added todo

--- compress.rb
+++ compress.rb
@@ -2,6 +2,22 @@ require 'RMagick'
 require 'zlib'
 include Magick
 
+
+# todo
+# 1 split rectangle into right isoceles triangles
+# - Euclidean algorithm does this
+# - http://arxiv.org/pdf/math/9411215.pdf
+# - lets use that
+# 2 number pixels in said triangle
+# - could be a better way to to this (better than naive)
+# 3 keep track of pixels while going through
+# - for each triangle, populate an array with each new pixel reached
+# 4 store meta-information
+# - basically just channels, depth, size
+# 5 compress route table into actual binary
+# - this may require a new language that works with binary
+# 6 compress delta table into minimum size
+
 # loads image
 # can change this to ARG[0] or whatever
 img = Image.read(ARGV[0]).first
@@ -20,18 +36,17 @@ delta_table = []
 # this is just for shits and giggles
 size_table  = []
 
-
 channels.each do |channel|
 
   index = {x: 0, y: 0}
   channel_delta_table = []
-  
+
   (0..(rows-1)).each do
-  
+
     value = img.pixel_color(index[:x], index[:y]).send(channel)
     left  = img.pixel_color(index[:x]+1, index[:y]).send(channel)
     right = img.pixel_color(index[:x], index[:y]+1).send(channel)
-  
+
     if right < left
       route_table << "0"
       channel_delta_table << right - value
@@ -42,24 +57,24 @@ channels.each do |channel|
       index[:x] += 1
     end
   end
-  
+
   delta_table << channel_delta_table
   size_table  << channel_delta_table.uniq.size
   #route_table << channel_route_table
-  
+
 end
 
 #puts "route table: #{route_table}"
 #puts route_table.to_s
 compressed_route_table = Zlib::Deflate.deflate(route_table)
-compressed_delta_table = Zlib::Deflate.deflate(delta_table.flatten.pack("c*"))
+compressed_delta_table = Zlib::Deflate.deflate(delta_table.flatten.pack("S*"))
 #puts compressed_route_table
 puts "size of delta table: #{size_table.to_s}"
 puts "combined size of delta tables: #{delta_table.flatten.uniq.size}"
 puts "size of compressed delta table: #{compressed_delta_table.bytesize} bytes"
 puts "size of compressed route table: #{compressed_route_table.bytesize} bytes"
 
-triangle_bytes = compressed_delta_table.bytesize +     
+triangle_bytes = compressed_delta_table.bytesize +
                  compressed_route_table.bytesize
 
 estimated_size_of_triangle = triangle_bytes * rows / 2
